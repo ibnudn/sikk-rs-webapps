70bd1db14d2aab64cd11373f5fc15dcad2b179b2
Ibnu Dwipa Nugraha <ibnudn@student.uns.ac.id>
2022-06-24 19:50:09 +0700

    change base_url & db_name

--- v2/application/config/config.php
+++ v2/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/ta/v2';
+$config['base_url'] = 'http://m3118039.mhs.d3tiuns.com/v2';
 
 /*
 |--------------------------------------------------------------------------
